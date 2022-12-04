3e1ea82abd3bf5ebf1bfc54757b8a38edee29384
Arina <arina.verbovskaya@mail.ru>
2022-12-04 09:52:03 +0300

    add task Recursion

---
+++ task_Rec/Program.cs
@@ -0,0 +1,19 @@
+﻿// Дополнительная задача на рекурсию (необязательная)
+// Напишите программу для подсчета количества цифр в числе с помощью рекурсии.
+// 12345 -> 5
+
+
+int Rec(int n)
+{
+    if (n < 10)
+    {
+        return 1;
+    }
+    else
+    {
+        return Rec(n / 10) + 1;
+    }
+}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Rec(num));
